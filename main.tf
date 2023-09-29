5f0ab851d0182af39d0aef71a11cdba01f1c0b1d
Labmin.me <114257285+LabminMe@users.noreply.github.com>
2023-09-29 13:45:07 -0500

    11 simple s3 bucket (#14)
    * #11 Create an S3 bucket and install TF AWS provider
    
    * #11 Updated readme.md documentation for AWS S3 config changes.

--- main.tf
+++ main.tf
@@ -4,18 +4,33 @@ terraform {
       source = "hashicorp/random" 
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
+# Configure AWS Provider
+provider "aws" {
+  # Configuration options
+}
+
+# Configure the Random Provider
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
+  upper            = false
   special          = false
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+}
 output "random_bucket_name"{
     value = random_string.bucket_name.result
 }
